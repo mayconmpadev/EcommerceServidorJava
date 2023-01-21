2422fd4b6427d7f59e9ef5b53a886ecaa915a780
mayco <mayconmpa.dev@gmail.com>
2023-01-21 16:55:44 -0300

    atualização 01/21/2023

--- app/src/main/java/com/example/ecommerceservidorjava/activity/OrcarActivity.java
+++ app/src/main/java/com/example/ecommerceservidorjava/activity/OrcarActivity.java
@@ -2,15 +2,25 @@ package com.example.ecommerceservidorjava.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import com.example.ecommerceservidorjava.R;
+import com.example.ecommerceservidorjava.databinding.ActivityOrcarBinding;
 
 public class OrcarActivity extends AppCompatActivity {
+    private ActivityOrcarBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orcar);
+        binding = ActivityOrcarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.floatingActionButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), OrcarPecasActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
